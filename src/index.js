4b708c846990a9dfc24a2291809a551ea2d6dd6b
Lana <tiushkievich00@mail.ru>
2022-07-30 21:14:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) return Number([...n.toString()].slice(1).reverse().join(""));
+    return Number([...n.toString()].reverse().join(""));
+};
